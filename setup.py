95b65cd296afe6d560340d200745cb901f5a3436
Youfar <wakafar@gmail.com>
2015-12-16 19:24:53 +0900

    remove dir

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 setup(
 	name = "smith",
-	version = "0.1",
+	version = "1.0",
 	packages = find_packages(),
+	scripts = ['smith_waterman.py'],
 )
---
+++ wscript
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+APPNAME = 'smithW'
+VERSION = '1.0'
+
+top = '.'
+builddir = 'build'
+
+def configure(conf):
+
+def build(bld):
